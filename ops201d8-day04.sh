0e88e3265256387eb1178cdd31f306194c8788b0
ewhd <ewhd22@gmail.com>
2023-04-27 17:12:11 +0000

    changed some names

---
+++ ops201d8-day04.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Your Name
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+
+# Declaration of variables
+files=("f1.txt" "f2.txt" "f3.txt" "f4.txt" "f5.txt")
+
+# Declaration of functions
+
+
+# Main
+echo ${files[4]}
+echo ${files[3]}
+echo ${files[2]}
+echo ${files[1]}
+echo ${files[0]}
+
+
+
+# End
