5df85c28bc8d975e0ea63b4dbfd82e700e0c5319
Ethan Denny <ewhd22@gmail.com>
2023-04-28 14:27:28 -0700

    Update ops201d8-day04.sh

--- ops201d8-day04.sh
+++ ops201d8-day04.sh
@@ -11,12 +11,7 @@ files=("f1.txt" "f2.txt" "f3.txt" "f4.txt" "f5.txt")
 # Declaration of functions
 
 
-# Main
-echo ${files[4]}
-echo ${files[3]}
-echo ${files[2]}
-echo ${files[1]}
-echo ${files[0]}
+# Make some other changes
 
 
 
